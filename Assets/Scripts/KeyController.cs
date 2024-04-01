d1097066440d9e82a9bd94acf4340ac12db80e13
agarish25 <159200376+agarish25@users.noreply.github.com>
2024-04-01 01:15:31 -0700

    fixed door and key

--- Assets/Scripts/KeyController.cs
+++ Assets/Scripts/KeyController.cs
@@ -7,7 +7,9 @@ public class KeyController : MonoBehaviour
 
     bool goUp = false;
     bool floating = true;
-    bool hasKey = false;
+
+    [SerializeField]
+    public bool hasKey = false;
     // Start is called before the first frame update
     void Start()
     {
