d5cc41e6be110625817df139f22332ee690b895f
agarish25 <159200376+agarish25@users.noreply.github.com>
2024-04-14 19:34:16 -0700

    created start screen

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public TextMeshProUGUI titleText;
+    public Button startGameButton;
+    public RectTransform blankPanel;
+    public bool isGameActive;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame() {
+        isGameActive = true;
+        titleText.gameObject.SetActive(false);
+        startGameButton.gameObject.SetActive(false);
+        blankPanel.gameObject.SetActive(false);
+    }
+}
